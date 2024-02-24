e5796489a3385aec2c97926adb293d484705c68b
Ivan Voras <ivoras@gmail.com>
2024-02-24 23:17:48 +0100

    New file confirmation if there are unsaved changes

--- handle_newfile.go
+++ handle_newfile.go
@@ -1,5 +1,18 @@
 package main
 
+import "fyne.io/fyne/v2/dialog"
+
 func (win *ENWindow) handleNewFile() {
-	win.Reset()
+	if win.isChanged {
+		dialog.ShowConfirm("Save document?",
+			"There are unsaved changes in the document. Do you wish to save the document?",
+			func(b bool) {
+				if b {
+					win.Reset()
+				}
+			},
+			win.win)
+	} else {
+		win.Reset()
+	}
 }
